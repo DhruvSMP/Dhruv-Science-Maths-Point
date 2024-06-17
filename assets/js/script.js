09bfbb004cc3ee53b5e2668776e8570220de54d6
yash149 <76114925+yash149@users.noreply.github.com>
2024-06-17 22:22:13 +0530

    commiting index and other pages
    made changes in every page and almost perfected index.html added pages for every classes there are and linked hem from index.html to respective pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,18 @@ const closeNavbar = function () {
 }
 
 addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/*=============== SEARCH BAR JS ===============*/
+const toggleSearch = (search, button) =>{
+  const searchBar = document.getElementById(search),
+        searchButton = document.getElementById(button)
+
+  searchButton.addEventListener('click', () =>{
+      // We add the show-search class, so that the search bar expands
+      searchBar.classList.toggle('show-search')
+  })
+}
+toggleSearch('search-bar', 'search-button')
+
